ca4a94272456b82bfe006e9d6a2c7e70e8f9a30b
nick stilwell <nickstilwell52@gmail.com>
2023-07-04 09:10:00 +0000

    total assignment submissions for each cohort

---
+++ 3_queries_group-by/total_assignment_submissions.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name AS cohort
+     , count(assignment_submissions.*) AS total_submissions
+FROM cohorts
+JOIN students ON cohort_id = cohorts.id
+JOIN assignment_submissions ON student_id = students.id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
