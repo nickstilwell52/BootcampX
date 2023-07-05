a73d12e3a7f5fe86b43253f61c00bafa7de55a0f
nick stilwell <nickstilwell52@gmail.com>
2023-07-05 01:17:46 +0000

    total average of the per cohort average assistance duration

---
+++ 4_queries/cohort_average_assistance_duration.sql
@@ -0,0 +1,10 @@
+SELECT avg(total_duration) as average_total_duration
+FROM (
+      SELECT cohorts.name as cohort
+           , SUM(completed_at - started_at) as total_duration
+      FROM cohorts
+      JOIN students ON cohorts.id = cohort_id
+      JOIN assistance_requests ON student_id = students.id
+      GROUP BY cohorts.name
+      ORDER BY total_duration
+) as average_durations;
